91d2e8cbb75d0b2535932006f2bcdfc75cabaad4
Thiago Mohr da Silveira <thiagomohrs@gmail.com>
2018-05-03 23:37:16 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,14 +8,30 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to opr multas ALPHA</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Login:
         </p>
+        <div className="App">
+        <form className="login">
+        <input placeholder="Login:" type="text" required/>
+        <input placeholder="Senha" type="password" required/>
+        <button onClick="validateForm()">Entrar</button>
+        </form>
+        </div>
       </div>
     );
   }
+}
+
+function validateForm(){
+  alert(document.forms);
+  var form = document.forms["login"].value;
+  if (form === "") {
+        alert("Form must be filled out");
+        return false;
+    }
 }
 
 export default App;
